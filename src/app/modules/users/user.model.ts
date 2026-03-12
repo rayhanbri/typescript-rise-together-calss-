073c679ecfd8011bbe2be842f0d222ae5ad85f2a
harshawellness <wellnesswithharsha10@gmail.com>
2026-03-12 07:11:37 +0600

    user.model.ts added

---
+++ src/app/modules/users/user.model.ts
@@ -0,0 +1,11 @@
+import { model, Schema } from "mongoose";
+import { IUser } from "./user.Interface";
+
+
+const userSchema=new Schema<IUser>({
+    name :{type: String},
+    email:{type : String},
+    password:{type : String}
+})
+
+const User =model<IUser>('user' , userSchema)
